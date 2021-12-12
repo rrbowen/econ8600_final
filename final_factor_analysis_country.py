138520812ecb1c76c88e8c57788de3d6a397efbf
Ross Bowen <42652813+rrbowen@users.noreply.github.com>
2021-12-12 17:25:03 -0500

    includingUS

--- final_factor_analysis_country.py
+++ final_factor_analysis_country.py
@@ -3,16 +3,16 @@ import numpy
 from factor_analyzer import FactorAnalyzer
 import matplotlib.pyplot as pyplot
 
-dataset = pandas.read_csv("dataset_final.csv")
+dataset = pandas.read_csv("dataset_final_usonly.csv")
 print(dataset)
 print(dataset.shape)
 
 #dataset.drop(['country'], axis=1, inplace = True)
-dataset.drop(['Unnamed: 0'],axis=1, inplace = True)
-dataset.dropna(inplace = True)
+#dataset.drop(['Unnamed: 0'],axis=1, inplace = True)
+#dataset.dropna(inplace = True)
 
-#print(dataset)
-#print(dataset.shape)
+print(dataset)
+print(dataset.shape)
 
 machine = FactorAnalyzer(n_factors=41, rotation = None)
 machine.fit(dataset)
@@ -20,7 +20,7 @@ ev, v = machine.get_eigenvalues()
 #print(ev)
 
 pyplot.scatter(range(1,dataset.shape[1]+1),ev)
-pyplot.savefig("plot_country.png")
+pyplot.savefig("plot_us.png")
 pyplot.close()
 
 machine = FactorAnalyzer(n_factors=3, rotation = 'Varimax')
@@ -37,13 +37,13 @@ dataset = dataset.values
 
 result = numpy.dot(dataset, loadings)
 
-print(result_country)
+print(result)
 print(result.shape)
 
-#"result" is the dataset we will use for clustering.  It does not include country for now
+#"result" is the dataset we will use for clustering.  US only
 
 from numpy import asarray
 from numpy import savetxt
-savetxt('result.csv', result, delimiter=',')
+savetxt('result_us.csv', result, delimiter=',')
 
 
---
+++ final_run_gmm_usonly.py
@@ -0,0 +1,38 @@
+import pandas
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+
+import matplotlib.pyplot as pyplot
+
+from sklearn.metrics import silhouette_score
+
+data = pandas.read_csv("result_us.csv")
+
+print(data)
+
+data = data.values
+
+pyplot.scatter(data[:,1], data[:,2])
+pyplot.savefig("scatter_gmm_US.png")
+pyplot.close()
+
+#data = data[:,1:3]
+
+#print(data)
+
+
+def run_gmm(n, data):
+	gmm_machine = GaussianMixture(n_components=n)
+	gmm_results = gmm_machine.fit_predict(data)
+	silhouette = 0
+	if n > 1:
+		silhouette = silhouette_score(data, gmm_results, metric = 'euclidean')
+	pyplot.scatter(data[:,0], data[:,1], c=gmm_results)
+	pyplot.savefig("scatter_gmm_US_" + str(n) + ".png")
+	pyplot.close()
+	return silhouette
+
+
+gmm_silhouette_list_us = [ run_gmm(i+1, data) for i in range(7)]
+print(gmm_silhouette_list_us)
+
